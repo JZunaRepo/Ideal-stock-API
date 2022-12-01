ada44955b3524b50d6857c8e917857ca2a23c09a
Jonnathan Zuna <Bingbong0201@github.com>
2022-11-30 20:52:41 -0500

    first

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-key = uKlVSSE4NJOAlgd0w7VkF73Kv3xXSzS3
+key = 'uKlVSSE4NJOAlgd0w7VkF73Kv3xXSzS3'
--- extract.py
+++ extract.py
@@ -1,8 +1,12 @@
-import time
 import requests
 from config import key
 
-stocks = []
+stocks = ["AAPL"]
 
 for stock in stocks:
   url = f"https://api.polygon.io/v2/aggs/ticker/{stock}/range/1/day/2021-03-29/2022-05-25?adjusted=true&sort=asc&limit=300&apiKey={key}"
+  data = requests.get(url)
+  print(data)
+  json_data = data.json()
+  results_data = json_data["results"]
+  print(results_data)
